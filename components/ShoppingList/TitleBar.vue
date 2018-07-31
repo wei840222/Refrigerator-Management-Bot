75b4b055ba070ce6cc6ff82e55b7d7b3cab7f51f
wei-wan <wei.wan@linecorp.com>
2018-07-31 18:14:22 +0800

    update titleBar

--- components/ShoppingList/TitleBar.vue
+++ components/ShoppingList/TitleBar.vue
@@ -1,8 +1,15 @@
 <template>
   <b-container>
-    <b-nav>
-      <b-nav-item active>Edit</b-nav-item>
-      <b-nav-item disabled>All</b-nav-item>
-    </b-nav>
+    <b-navbar toggleable="md">
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item href="#">編輯</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right>
+          <b-nav-item href="#">看全部</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </b-container>
 </template>
