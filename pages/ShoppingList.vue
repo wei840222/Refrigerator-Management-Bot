fb85c25b6bf30c96630867ddb632d84380558f62
wei-wan <wei.wan@linecorp.com>
2018-07-31 17:52:25 +0800

    add many food data

--- pages/ShoppingList.vue
+++ pages/ShoppingList.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
     <title-bar/>
-    <food-block :type="data.type" :foods="data.foodArray" />
+    <food-block  v-for="foodData in data" :key="foodData.type" :type="foodData.type" :foods="foodData.foodArray" />
   </b-container>
 </template>
 
@@ -10,17 +10,27 @@ import TitleBar from "~/components/ShoppingList/TitleBar.vue";
 import FoodBlock from "~/components/ShoppingList/FoodBlock.vue";
 
 export default {
-  async asyncData() {
-    return {
-      data: {
-        type: "菜",
-        foodArray: [{ id: 1, name: "高麗菜" }, { id: 2, name: "花菜" }]
-      }
-    };
-  },
   head() {
     return {
       title: "Shopping List"
+    };
+  },
+  async asyncData() {
+    return {
+      data: [
+        {
+          type: "菜",
+          foodArray: [{ id: 1, name: "高麗菜" }, { id: 2, name: "花菜" }]
+        },
+        {
+          type: "肉",
+          foodArray: [
+            { id: 3, name: "牛五花" },
+            { id: 4, name: "梅花豬" },
+            { id: 5, name: "小羔羊" }
+          ]
+        }
+      ]
     };
   },
   components: {
