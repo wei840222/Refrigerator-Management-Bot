657b259f047c815a97555da785c31a95a99564f4
wei-wan <wei.wan@linecorp.com>
2018-07-31 18:15:26 +0800

    fix RecommendRow not defined

--- components/ShoppingList/RecommendBlock.vue
+++ components/ShoppingList/RecommendBlock.vue
@@ -1,14 +1,14 @@
 <template>
   <b-container>
     <b-card bg-variant="dark" text-variant="white" :title="type">
-      <food-row v-for="food in foods" :key="food.id"  :id="food.id" :name="food.name">
+      <recommend-row v-for="food in foods" :key="food.id"  :id="food.id" :name="food.name">
     </b-card>
     <br/>
   </b-container>
 </template>
 
 <script>
-import FoodRow from "~/components/ShoppingList/RecommendRow.vue";
+import RecommendRow from "~/components/ShoppingList/RecommendRow.vue";
 
 export default {
   props: {
