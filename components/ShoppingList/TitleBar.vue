156ec2261bf236a569d1e22a07a41435e2334a48
wei-wan <wei.wan@linecorp.com>
2018-07-31 18:40:43 +0800

    try fix title bar

--- components/ShoppingList/TitleBar.vue
+++ components/ShoppingList/TitleBar.vue
@@ -1,15 +1,6 @@
 <template>
   <b-container>
-    <b-navbar toggleable="md">
-      <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
-          <b-nav-item href="#">編輯</b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item right>
-          <b-nav-item href="#">看全部</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <b-button>編輯</b-button>
+    <b-button>全部</b-button>
   </b-container>
 </template>
