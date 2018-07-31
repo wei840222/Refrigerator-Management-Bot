fee128fdb313a8ae5d4f0678d35163163139916b
wei-wan <wei.wan@linecorp.com>
2018-07-31 18:50:59 +0800

    titlebar

--- components/ShoppingList/TitleBar.vue
+++ components/ShoppingList/TitleBar.vue
@@ -3,7 +3,7 @@
     <br/>
     <b-row>
       <b-col><b-button>編輯</b-button></b-col>
-      <b-col cols="5"></b-col>
+      <b-col cols="6"></b-col>
       <b-col><b-button>全部</b-button></b-col>
     </b-row>
     <br/>
