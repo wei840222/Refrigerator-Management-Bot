0483828264939c92577a1fec18da8a7b64a1fd79
wei-wan <wei.wan@linecorp.com>
2018-07-31 19:02:13 +0800

    titlebar

--- components/ShoppingList/TitleBar.vue
+++ components/ShoppingList/TitleBar.vue
@@ -4,7 +4,7 @@
     <b-row align-h="center">
       <b-col><b-button>編輯</b-button></b-col>
       <b-col cols="5"></b-col>
-      <b-col><b-button>全部</b-button></b-col>
+      <b-col><b-button style="float:right;">全部</b-button></b-col>
     </b-row>
     <br/>
   </b-container>
