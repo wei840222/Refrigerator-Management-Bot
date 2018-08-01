85e3f431acffc914f734ecc0805166852805b92e
萬俊瑋 <wei840222@gmail.com>
2018-08-01 21:46:12 +0800

    add RefrigeratorList

---
+++ components/RefrigeratorList/FoodBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-card-body>
+    <food-row v-for="foodData in foods" :key="foodData.id" :id="foodData.id" :name="foodData.name" :type="foodData.type" :buyTime="foodData.buyTime" :period="foodData.period"/>
+  </b-card-body>
+</template>
+
+<script>
+import FoodRow from "~/components/RefrigeratorList/FoodRow.vue";
+
+export default {
+  props: {
+    foods: Array
+  },
+  components: {
+    FoodRow
+  }
+};
+</script>
