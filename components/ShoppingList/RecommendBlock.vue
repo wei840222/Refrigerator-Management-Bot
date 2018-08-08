163505d36550e4e4c7da2c7df584e9ab4dff2987
wei-wan <wei.wan@linecorp.com>
2018-08-08 16:44:43 +0800

    update shoplist RecommendBlock

---
+++ components/ShoppingList/RecommendBlock.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="title" v-b-toggle.recommend>
+      <div class="title-text">建議清單</div>
+    </div>
+    <b-collapse visible id="recommend">
+      <div class="food" v-for="food in foodData" :key="food.id">{{ food.name }}</div>
+    </b-collapse>
+  </div>
+</template>
+
+<style>
+.title {
+  height: 30px;
+  width: 100%;
+  color: #949394;
+  background-color: #e3e2e2;
+}
+.title-text {
+  padding-left: 40px;
+  padding-top: 3px;
+}
+.food {
+  width: 100%;
+  padding-top: 2px;
+  padding-left: 40px;
+  background-color: #f6f6f6;
+}
+.edit {
+  width: 100%;
+  padding-top: 2px;
+  padding-left: 40px;
+  background-color: #f6f6f6;
+}
+</style>
+
+
+<script>
+export default {
+  props: {
+    foodData: Array
+  }
+};
+</script>
--- pages/ShoppingList.vue
+++ pages/ShoppingList.vue
@@ -2,6 +2,7 @@
   <div>
     <title-bar class="title-bar"/>
     <food-block class="food-block" v-for="foodData in data" :key="foodData.type" :foodData="foodData"/>
+    <recommend-block class="recommend-block" :foodData="recommend"/>
   </div>
 </template>
 
@@ -15,11 +16,16 @@
   position: fixed;
   z-index: 1;
   background-color: #ffffff;
-  box-shadow:0px 1px 2px rgba(20%,20%,40%,0.5);
+  box-shadow: 0px 1px 2px rgba(20%, 20%, 40%, 0.5);
 }
 .food-block {
   position: relative;
-  top:40px;
+  top: 40px;
+  width: 100%;
+}
+.recommend-block {
+  position: relative;
+  margin-top: 40px;
   width: 100%;
 }
 </style>
@@ -27,6 +33,7 @@
 <script>
 import TitleBar from "~/components/ShoppingList/TitleBar.vue";
 import FoodBlock from "~/components/ShoppingList/FoodBlock.vue";
+import RecommendBlock from "~/components/ShoppingList/RecommendBlock.vue";
 
 export default {
   head() {
@@ -39,10 +46,7 @@ export default {
       data: [
         {
           type: "菜",
-          foodArray: [
-            { id: "1", name: "高麗菜" },
-            { id: "2", name: "花菜" }
-          ]
+          foodArray: [{ id: "1", name: "高麗菜" }, { id: "2", name: "花菜" }]
         },
         {
           type: "肉",
@@ -57,23 +61,22 @@ export default {
           foodArray: [
             { id: "6", name: "鱈魚" },
             { id: "7", name: "虱目魚" },
-            { id: "8", name: "鯊魚" }
-          ]
-        },
-        {
-          type: "建議",
-          foodArray: [
-            { id: "9", name: "巧克力" },
-            { id: "10", name: "棉花糖" },
-            { id: "11", name: "奶油餅" }
+            { id: "8", name: "鯊魚" },
+            { id: "9", name: "鮭魚" }
           ]
         }
+      ],
+      recommend: [
+        { id: "1", name: "巧克力" },
+        { id: "2", name: "棉花糖" },
+        { id: "3", name: "奶油餅" }
       ]
     };
   },
   components: {
     TitleBar,
-    FoodBlock
+    FoodBlock,
+    RecommendBlock
   }
 };
 </script>
