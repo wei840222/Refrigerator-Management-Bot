4727cb00ebbe33eadad9cf9c9de99f7216a73bdf
wei-wan <wei.wan@linecorp.com>
2018-08-10 11:38:54 +0800

    update shoopingList recommandBlock

--- components/ShoppingList/RecommendBlock.vue
+++ components/ShoppingList/RecommendBlock.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <div class="recommend-title" v-b-toggle.recommend>
+    <div class="recommend-title" v-b-toggle.recommend @click="collapsed =!collapsed">
       <div class="recommend-title-text">建議清單</div>
+      <img v-if="collapsed" src="arrow-down.png" class="food-title-icon"/>
+      <img v-else src="arrow-up.png" class="food-title-icon"/>
     </div>
     <b-collapse visible id="recommend">
-      <b-form-checkbox
-        class="recommend-item"
-        v-for="rmd in recommend"
-        :key="rmd.id"
-        :id="rmd.id"
-        v-model="rmd.select"
-        :style="{ 'text-decoration':rmd.select ? 'line-through black' : '' }">
-        {{ rmd.name }}
-      </b-form-checkbox>
+      <div class="recommend-item" v-for="rmd in recommend" :key="rmd.id" @click="rmd.select = !rmd.select">
+        <img v-if="rmd.select" src="ckeck-box-act.png" class="food-item-check-box"/>
+        <img v-else src="check-box.png" class="food-item-check-box"/>
+        <div :style="{ 'flex-grow': 1, 'text-decoration': rmd.select ? 'line-through black' : '' }">{{ rmd.name }}</div>
+        <img v-if="edit" src="del.png" class="food-item-del"/>
+      </div>
     </b-collapse>
   </div>
 </template>
@@ -21,6 +20,11 @@
 export default {
   props: {
     recommend: Array
+  },
+  data() {
+    return {
+      collapsed: false
+    };
   }
 };
 </script>
