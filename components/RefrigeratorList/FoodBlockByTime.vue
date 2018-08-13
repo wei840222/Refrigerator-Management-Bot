2dbbb88a772e19264d6bab2bf7b612a831fd9237
wei-wan <wei.wan@linecorp.com>
2018-08-13 17:50:44 +0800

    update edit RefrigeratorList data

--- components/RefrigeratorList/FoodBlockByTime.vue
+++ components/RefrigeratorList/FoodBlockByTime.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
-    <div class="food-title" v-b-toggle="title" style="background-color: #eae8e8" @click="collapsed =!collapsed">
+    <div class="food-title" v-b-toggle="title + 'collapse'" style="background-color: #eae8e8" @click="collapsed =!collapsed">
       <div class="food-title-text" style="color: #b7b7b8">{{ title }}</div>
       <img v-if="collapsed" src="arrow-down.png" class="food-title-icon"/>
       <img v-else src="arrow-up.png" class="food-title-icon"/>
     </div>
-    <b-collapse :visible="collapseVisible" :id="title">
+    <b-collapse :visible="collapseVisible" :id="title + 'collapse'">
       <div class="food-item" v-for="(food, idx) in foods" :key="idx">
         <b-badge variant="primary" class="food-item-type">{{ food.type }}</b-badge>
         <div class="food-item-text1">{{ food.nameZh }}</div>
         <div class="food-item-text3">{{ dateAdd(food.acquisitionDate, food.expirationDate) }}</div>
-        <img src="edit.png" class="food-item-del" v-b-modal="'add'"/>
-        <b-modal id="add" title="新增品項" @ok="addItem">
+        <img src="edit.png" class="food-item-del" v-b-modal="(food.acquisitionDate + idx).toString()"/>
+        <b-modal :id="(food.acquisitionDate + idx).toString()" title="編輯品項" ok-only :ok-title="'完成'" @hidden="updateItem(food)">
           名稱：<br/>
           <b-form-input v-model="food.nameZh" type="text"></b-form-input><br/>
           品項：<br/>
           <b-form-input v-model="food.type" type="text"></b-form-input><br/>
           購買日期：<br/>
           <b-form-input v-model="food.acquisitionDate" type="date"></b-form-input><br/>
-          保存期限：<br/>
+          保存期限（天）：<br/>
           <b-form-input v-model="food.expirationDate" type="number"></b-form-input>
         </b-modal>
       </div>
@@ -27,6 +27,8 @@
 </template>
 
 <script>
+import axios from "~/plugins/axios";
+
 export default {
   props: {
     title: String,
@@ -54,6 +56,16 @@ export default {
         "-" +
         (date[2].length === 1 ? "0" + date[2] : date[2])
       );
+    },
+    async updateItem(food) {
+      const data = {
+        id: food.id,
+        nameZh: food.nameZh,
+        type: food.type,
+        acquisitionDate: food.acquisitionDate,
+        expirationDate: this.dateAdd(food.acquisitionDate, food.expirationDate)
+      };
+      const res = await axios.post("/cabinet/userId/edit_item", data);
     }
   }
 };
