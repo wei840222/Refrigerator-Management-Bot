288d6e4f48effc70125744ea1cf92597b3697a95
萬俊瑋 <wei840222@gmail.com>
2018-08-20 23:54:10 +0800

    refactory ShoppingList

--- components/FoodBlock.vue
+++ components/FoodBlock.vue
@@ -1,259 +1,64 @@
 <template>
-  <div>
-    <div v-if="type != 'all'" class="food-title" v-b-toggle="type" @click="collapsed =!collapsed">
-      <img :src="titleTypeImg" class="food-title-type"/>
-      <div class="food-title-text">
-        <div class="food-title-text-ch">{{ type }}</div>
-        <div class="food-title-text-eng">{{ titleEnglish }}</div>
-      </div>
-      <div v-if="foods.length > 0">
-        <img v-if="collapsed" src="img/ShoppingList/arrow-green.png" class="food-title-icon"/>
-        <img v-else src="img/ShoppingList/arrow-green.png" class="food-title-icon"/>
-      </div>
+  <div class="food-block">
+    <div class="header">
+      <div class="icon"><slot name="icon"/></div>
+      <div class="text"><slot name="text"/></div>
+      <div class="arrow"><slot name="arrow"/></div>
     </div>
-    <div v-else class="food-title-all"/>
-    <b-collapse visible :id="type">
-      <div class="food-item" v-for="(food, idx) in foods" :key="idx">
-        <img v-if="food.selected" src="img/ShoppingList/check-box-act.png" class="food-item-check-box" @click="food.selected = !food.selected"/>
-        <img v-else src="img/ShoppingList/check-box.png" class="food-item-check-box" @click="food.selected = !food.selected"/>
-        <div :style="{ 'flex-grow': 1, 'text-decoration': food.selected ? 'line-through black' : '' }" @click="food.selected = !food.selected">{{ food.nameZh }}</div>
-        <img v-if="edit" src="img/ShoppingList/food-del.png" class="food-item-del" @click="delFood(arguments, food)"/>
-      </div>
-    </b-collapse>
-    <div v-if="edit" class="edit">
-      <img src="img/ShoppingList/food-add.png" class="edit-add"/>
-      <div class="edit-input-box">
-        <input class="edit-input" v-model="addFoodName" type="text" placeholder="新增" @change="addFood"/>
-      </div>
-      <img src="img/ShoppingList/food-add-fin.png" class="food-item-add-fin" @click="addFood"/>
-    </div>
-    <div class="food-footer"/>
+    <div class="content"><slot/></div>
   </div>
 </template>
 
-<style>
-.food-title {
+<style lang="scss" scoped>
+.food-block {
+  position: relative;
+  top: 40px;
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  border-radius: 15px 15px 0 0;
-  background-color: #ffffff;
-  box-shadow: 1px 2px 1px #8a8a8a;
-}
+  margin-top: 21px;
+  padding-right: 16.8px;
+  padding-left: 54.3px;
 
-.food-title-all {
-  width: 100%;
-  height: 15px;
-  margin-bottom: -1px;
-  border-radius: 15px 15px 0 0;
-  background-color: #ffffff;
-  box-shadow: 1px 2px 1px #8a8a8a;
-}
+  .header {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    border-radius: 10px 10px 0 0;
+    background-color: #ffffff;
+    box-shadow: 2px 2px 3px 1px #8a8a8a;
+    z-index: 1;
 
-.food-title-type {
-  height: 63px;
-  width: 63px;
-  margin-left: -28px;
-  margin-top: 16px;
-  border-radius: 31.5px;
-  box-shadow: 1px 2px 1px #8a8a8a;
-  background-color: #ffffff;
-}
-
-.food-title-text {
-  margin-left: 40px;
-  margin-top: 21.6px;
-  margin-bottom: 21.6px;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-}
-
-.food-title-text-ch {
-  margin-bottom: 6.6px;
-  color: #6d6d6d;
-  font-size: 19.6px;
-}
-
-.food-title-text-eng {
-  color: #999899;
-  font-size: 12.9px;
-}
-
-.food-title-icon {
-  height: 12px;
-  width: 21px;
-  margin-top: 42.89px;
-  margin-right: 17.6px;
-}
-
-.food-item {
-  width: 100%;
-  height: 38px;
-  padding-left: 40px;
-  padding-top: 9px;
-  color: #8a8a8a;
-  background-color: #ffffff;
-  border-top: 1px #e8e8e8 solid;
-  box-shadow: 1px 2px 1px #8a8a8a;
-  display: flex;
-}
-
-.food-item-check-box {
-  height: 14px;
-  width: 14px;
-  margin-top: 4px;
-  margin-right: 10px;
-}
-
-.food-item-del {
-  height: 15px;
-  width: 15px;
-  margin-top: 3px;
-  margin-right: 23px;
-  z-index: 1;
-}
-
-.edit {
-  width: 100%;
-  height: 40px;
-  padding-left: 38px;
-  color: #d2d6da;
-  background-color: #ffffff;
-  box-shadow: 1px 2px 1px #8a8a8a;
-  border-top: 1px #e8e8e8 dashed;
-  display: flex;
-}
-
-.edit-add {
-  height: 17px;
-  width: 17px;
-  margin-top: 11px;
-  margin-right: 8px;
-  z-index: 1;
-}
-
-.edit-input-box {
-  flex-grow: 1;
-}
-
-.edit-input {
-  border-width: 0px;
-  height: 30px;
-  width: 100px;
-  margin-left: 0px;
-  margin-top: 4px;
-  margin-right: 10px;
-  caret-color: #cdd2d6;
-  color: #cdd2d6;
-  background-color: #ffffff;
-  outline: none;
-}
-
-.food-item-add-fin {
-  height: 20px;
-  width: 48px;
-  min-width: 48px;
-  margin-top: 9px;
-  margin-right: 17.6px;
-  z-index: 1;
-}
-
-.food-footer {
-  height: 15px;
-  width: 100%;
-  border-radius: 0 0 15px 15px;
-  background-color: #ffffff;
-  box-shadow: 1px 2px 1px #8a8a8a;
-}
-</style>
-
-<script>
-import axios from "~/plugins/axios";
-
-export default {
-  props: {
-    type: String,
-    foods: Array,
-    edit: Boolean
-  },
-  data() {
-    return {
-      editing: false,
-      addFoodName: "",
-      editText: "新增",
-      collapsed: false
-    };
-  },
-  computed: {
-    titleEnglish() {
-      switch (this.type) {
-        case "冷凍":
-          return "Frozen Food";
-        case "飲料":
-          return "Drinks";
-        case "青菜":
-          return "Vegetable";
-        case "肉":
-          return "Meat";
-        case "海鮮":
-          return "Seafood";
-        case "甜品":
-          return "Sweet";
-        case "零食":
-          return "Snack";
-        case "水果":
-          return "Fruit";
-        case "其他":
-          return "Others";
-      }
-    },
-    titleTypeImg() {
-      switch (this.type) {
-        case "冷凍":
-          return "img/ShoppingList/type-frozen-food.png";
-        case "飲料":
-          return "img/ShoppingList/type-drinks.png";
-        case "青菜":
-          return "img/ShoppingList/type-vegetable.png";
-        case "肉":
-          return "img/ShoppingList/type-meat.png";
-        case "海鮮":
-          return "img/ShoppingList/type-seafood.png";
-        case "甜品":
-          return "img/ShoppingList/type-sweet.png";
-        case "零食":
-          return "img/ShoppingList/type-sweet.png";
-        case "水果":
-          return "img/ShoppingList/type-fruit.png";
-        case "其他":
-          return "img/ShoppingList/type-others.png";
-      }
+    .icon {
+      height: 63px;
+      width: 63px;
+      margin-left: -28px;
+      margin-top: 10px;
+      border-radius: 31.5px;
+      box-shadow: 2px 2px 2px #8a8a8a;
+      background-color: #ffffff;
     }
-  },
-  methods: {
-    addFood() {
-      this.editText = "新增";
-      if (this.addFoodName != "") {
-        this.$emit("addFood", {
-          type: this.type,
-          addFoodName: this.addFoodName
-        });
-        this.editing = false;
-        this.addFoodName = "";
-      }
-    },
-    async delFood(event, food) {
-      const res = await axios.post(
-        "/cabinet/userId/delete_item_from_shoppingist",
-        {
-          id: food.id,
-          nameZh: food.addFoodName,
-          type: food.type
-        }
-      );
-      if (res.status === 200) this.$emit("delFood");
+
+    .text {
+      margin-left: 20px;
+      margin-top: 21.6px;
+      margin-bottom: 21.6px;
+      flex-grow: 1;
+      color: #6d6d6d;
+      font-size: 19.6px;
+    }
+
+    .arrow {
+      height: 12px;
+      width: 21px;
+      margin-top: 25px;
+      margin-right: 17.6px;
     }
   }
-};
-</script>
+
+  .content {
+    width: 100%;
+    background-color: #ffffff;
+    border-radius: 0 0 10px 10px;
+    box-shadow: 2px 2px 2px#8a8a8a;
+  }
+}
+</style>
--- pages/ShoppingList.vue
+++ pages/ShoppingList.vue
@@ -1,26 +1,63 @@
 <template>
   <div>
     <div class="green-bar"/>
-    <div class="title-bar">
-      <div class="title-item" @click="selectedAll = !selectedAll; selectAll()" :style="{ 'color': selectedAll ? '#000000' : '#aaaaaa' }">全選</div>
-      <div class="title-item" @click="edit = !edit" :style="{ 'color': edit ? '#000000' : '#aaaaaa' }">編輯</div>
-      <div class="sort-option" @click="groupByType = !groupByType">
-        <div class="option-text">{{ groupByType ? '品項' : '全部'}}</div>
-        <img class="option-icon" src="option.png"/>
+    <div class="nav">
+      <div class="item" @click="selectedAll = !selectedAll; selectAll()" :style="{ 'color': selectedAll ? '#000000' : '#aaaaaa' }">全選</div>
+      <div class="item" @click="groupByType ? edit = !edit : ''" :style="{ 'color': edit ? '#000000' : '#aaaaaa' }">編輯</div>
+      <div class="option" @click="groupByType = !groupByType">
+        <div class="text">{{ groupByType ? '品項' : '全部'}}</div>
+        <img class="icon" src="option.png"/>
       </div>
     </div>
     <div v-if="groupByType">
-      <food-block class="food-block" v-for="(type, idx) in types" :key="idx" :edit="edit" :type="type" :foods="foods(type)" @addFood="addFood" @delFood="delFood"/>
+      <food-block  v-for="(type, idx) in types" :key="idx">
+        <img slot="icon" :src="foodBlockIconSrc(type)"/>
+        <span slot="text">{{ type }}</span>
+        <img slot="arrow" src="img/ShoppingList/arrow-green.png"/>
+        <div class="food" v-for="(food, idx) in foods(type)" :key="idx" :style="{ 'border-radius': !edit && idx === foods(type).length - 1 ? '0 0 10px 10px' : '' }">
+          <img v-if="food.selected" src="img/ShoppingList/check-box-act.png" class="check-box" @click="food.selected = !food.selected"/>
+          <img v-else src="img/ShoppingList/check-box.png" class="check-box" @click="food.selected = !food.selected"/>
+          <div :style="{ 'flex-grow': 1, 'text-decoration': food.selected ? 'line-through black' : '' }" @click="food.selected = !food.selected">{{ food.nameZh }}</div>
+          <img v-if="edit" src="img/ShoppingList/food-del.png" class="del" @click="delFood(food);"/>
+        </div>
+        <div v-if="edit" class="edit">
+          <img src="img/ShoppingList/food-add.png" class="add"/>
+          <div class="input-box">
+            <input class="input" type="text" placeholder="新增" v-model="addFood[type]" @change="addFood({ nameZh: addFood[type], type: type }); addFood[type] = '';"/>
+          </div>
+          <img src="img/ShoppingList/food-add-fin.png" class="fin" @click="addFood({ nameZh: addFood[type], type: type }); addFood[type] = '';"/>
+        </div>
+        <div v-if="!edit && foods(type).length === 0" style="height: 10px; width: 100%;"/>
+      </food-block>
     </div>
     <div v-else>
-      <food-block class="food-block" :edit="false" :type="'all'" :foods="foods('all')" @addFood="addFood"/>
+      <food-block>
+        <img slot="icon" src="img/ShoppingList/type-all.png"/>
+        <span slot="text">全部</span>
+        <div class="food" v-for="(food, idx) in foods()" :key="idx" :style="idx === foods().length - 1 ? 'border-radius: 0 0 10px 10px;' : ''">
+          <img v-if="food.selected" src="img/ShoppingList/check-box-act.png" class="check-box" @click="food.selected = !food.selected"/>
+          <img v-else src="img/ShoppingList/check-box.png" class="check-box" @click="food.selected = !food.selected"/>
+          <div :style="{ 'flex-grow': 1, 'text-decoration': food.selected ? 'line-through black' : '' }" @click="food.selected = !food.selected">{{ food.nameZh }}</div>
+          <img v-if="edit" src="img/ShoppingList/food-del.png" class="del" @click="delFood(food);"/>
+        </div>
+        <div v-if="foods().length === 0" style="height: 10px; width: 100%;"/>
+      </food-block>
     </div>
-    <recommend-block class="recommend-block" :foods="recommendationList.slice(0, 5)" @addRecommendFood="addRecommendFood"/>
-    <div class="footer"><img src="addToRefrigerator.png" class="footer-bar" @click="addToRefrigerator"/></div>
+    <food-block>
+      <img slot="icon" src="img/ShoppingList/type-all.png"/>
+      <span slot="text">建議</span>
+      <div class="food" v-for="(food, idx) in recommendationList.slice(0, 5)" :key="idx" :style="idx === recommendationList.slice(0, 5).length - 1 ? 'border-radius: 0 0 10px 10px; margin-bottom: 85px;' : ''">
+        <img v-if="food.selected" src="img/ShoppingList/check-box-act.png" class="check-box" @click="food.selected = !food.selected; addRecommendFood(idx);"/>
+        <img v-else src="img/ShoppingList/check-box.png" class="check-box" @click="food.selected = !food.selected"/>
+        <div :style="{ 'flex-grow': 1, 'text-decoration': food.selected ? 'line-through black' : '' }" @click="food.selected = !food.selected; addRecommendFood(idx);">{{ food.nameZh }}</div>
+      </div>
+      <div v-if="recommendationList.slice(0, 5).length === 0" style="height: 10px; width: 100%; margin-bottom: 85px;"/>
+    </food-block>
+    <div class="footer"><img src="addToRefrigerator.png" class="button" @click="addToRefrigerator"/></div>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 .green-bar {
   position: fixed;
   top: 0;
@@ -30,7 +67,7 @@
   background-color: #27ab38;
 }
 
-.title-bar {
+.nav {
   top: 0px;
   padding-left: 40px;
   padding-right: 20px;
@@ -41,57 +78,110 @@
   z-index: 4;
   background-color: #ffffff;
   box-shadow: 0px 1px 2px rgba(20%, 20%, 40%, 0.5);
+
+  .item {
+    width: 60px;
+    height: 20px;
+    margin-top: 10px;
+  }
+
+  .option {
+    margin-top: 11px;
+    height: 22px;
+    width: 60px;
+    border: #b7b7b8;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 11px;
+    margin-left: auto;
+    display: flex;
+
+    .text {
+      font-size: 12px;
+      margin-left: 12px;
+      margin-top: 2px;
+      color: #cecfcf;
+    }
+
+    .icon {
+      height: 5px;
+      width: 9px;
+      margin-top: 8px;
+      margin-left: 4px;
+    }
+  }
 }
 
-.title-item {
-  width: 60px;
-  height: 20px;
-  margin-top: 10px;
-}
-
-.sort-option {
-  margin-top: 11px;
-  height: 22px;
-  width: 60px;
-  border: #b7b7b8;
-  border-style: solid;
-  border-width: 1px;
-  border-radius: 11px;
-  margin-left: auto;
+.food {
+  width: 100%;
+  height: 38px;
+  padding-left: 40px;
+  padding-top: 9px;
+  color: #8a8a8a;
+  background-color: #ffffff;
+  box-shadow: 1px 2px 1px #8a8a8a;
   display: flex;
+
+  .check-box {
+    height: 14px;
+    width: 14px;
+    margin-top: 4px;
+    margin-right: 10px;
+  }
+
+  .del {
+    height: 15px;
+    width: 15px;
+    margin-top: 3px;
+    margin-right: 23px;
+    z-index: 1;
+  }
 }
 
-.option-text {
-  font-size: 12px;
-  margin-left: 12px;
-  margin-top: 2px;
-  color: #cecfcf;
-}
-
-.option-icon {
-  height: 5px;
-  width: 9px;
-  margin-top: 8px;
-  margin-left: 4px;
-}
-
-.food-block {
-  position: relative;
-  top: 40px;
+.edit {
   width: 100%;
-  margin-top: 21px;
-  padding-right: 16.8px;
-  padding-left: 54.3px;
-}
+  height: 40px;
+  padding-left: 38px;
+  color: #d2d6da;
+  background-color: #ffffff;
+  box-shadow: 2px 2px 2px#8a8a8a;
+  border-top: 1px #e8e8e8 dashed;
+  border-radius: 0 0 10px 10px;
+  display: flex;
 
-.recommend-block {
-  position: relative;
-  top: 40px;
-  width: 100%;
-  margin-top: 21px;
-  padding-right: 16.8px;
-  padding-left: 54.3px;
-  padding-bottom: 85px;
+  .add {
+    height: 17px;
+    width: 17px;
+    margin-top: 11px;
+    margin-right: 8px;
+    z-index: 1;
+  }
+
+  .input-box {
+    flex-grow: 1;
+  }
+
+  .input {
+    border-width: 0px;
+    height: 30px;
+    width: 100px;
+    margin-left: 0px;
+    margin-top: 4px;
+    margin-right: 10px;
+    caret-color: #cdd2d6;
+    color: #cdd2d6;
+    background-color: #ffffff;
+    outline: none;
+  }
+
+  .fin {
+    height: 20px;
+    width: 48px;
+    min-width: 48px;
+    margin-top: 9px;
+    margin-right: 17.6px;
+    z-index: 1;
+  }
 }
 
 .footer {
@@ -104,21 +194,20 @@
   background-color: #ffffff;
   justify-content: center;
   box-shadow: 0 0 5px rgba(20%, 20%, 40%, 0.5);
-}
 
-.footer-bar {
-  bottom: 0px;
-  margin-top: 12px;
-  margin-bottom: 12px;
-  height: 40px;
-  z-index: 10;
+  .button {
+    bottom: 0px;
+    margin-top: 12px;
+    margin-bottom: 12px;
+    height: 40px;
+    z-index: 10;
+  }
 }
 </style>
 
 <script>
 import axios from "~/plugins/axios";
-import FoodBlock from "~/components/ShoppingList/FoodBlock.vue";
-import RecommendBlock from "~/components/ShoppingList/RecommendBlock.vue";
+import FoodBlock from "~/components/FoodBlock.vue";
 
 export default {
   async asyncData() {
@@ -150,21 +239,32 @@ export default {
       edit: false,
       groupByType: true,
       selectedAll: false,
-      types: [
-        "肉",
-        "青菜",
-        "水果",
-        "海鮮",
-        "點心",
-        "飲料",
-        "冷凍",
-        "其他"
-      ]
+      types: ["肉", "青菜", "水果", "海鮮", "點心", "飲料", "冷凍", "其他"]
     };
   },
   methods: {
+    foodBlockIconSrc(type) {
+      switch (type) {
+        case "冷凍":
+          return "img/ShoppingList/type-frozen-food.png";
+        case "飲料":
+          return "img/ShoppingList/type-drinks.png";
+        case "青菜":
+          return "img/ShoppingList/type-vegetable.png";
+        case "肉":
+          return "img/ShoppingList/type-meat.png";
+        case "海鮮":
+          return "img/ShoppingList/type-seafood.png";
+        case "點心":
+          return "img/ShoppingList/type-snack.png";
+        case "水果":
+          return "img/ShoppingList/type-fruit.png";
+        case "其他":
+          return "img/ShoppingList/type-others.png";
+      }
+    },
     foods(type) {
-      if (type === "all") return this.shoppingItems;
+      if (!type) return this.shoppingItems;
       else return this.shoppingItems.filter(item => item.type === type);
     },
     selectAll() {
@@ -173,31 +273,42 @@ export default {
       });
     },
     async addFood(food) {
-      const res = await axios.post("/cabinet/userId/add_item_to_shoppingist", {
-        nameZh: food.addFoodName,
-        type: food.type
-      });
+      if (food.nameZh === "") return;
+      const res = await axios.post(
+        "/cabinet/userId/add_item_to_shoppingist",
+        food
+      );
       if (res.status === 200)
         this.shoppingItems.push({
           id: res.data,
-          nameZh: food.addFoodName,
+          nameZh: food.nameZh,
           type: food.type,
           selected: false
         });
     },
-    async delFood() {
-      const res = await axios.get("/cabinet/userId/shopping_list");
-      res.data.shoppingItems.forEach(element => {
-        element.selected = this.shoppingItems.filter(
-          item => item.id === element.id
-        ).selected;
-      });
-      this.shoppingItems = res.data.shoppingItems;
+    async delFood(food) {
+      let res = await axios.post(
+        "/cabinet/userId/delete_item_from_shoppingist",
+        {
+          id: food.id,
+          nameZh: food.nameZh,
+          type: food.type
+        }
+      );
+      if (res.status === 200) {
+        res = await axios.get("/cabinet/userId/shopping_list");
+        res.data.shoppingItems.forEach(element => {
+          element.selected = this.shoppingItems.filter(
+            item => item.id === element.id
+          ).selected;
+        });
+        this.shoppingItems = res.data.shoppingItems;
+      }
     },
     addRecommendFood(idx) {
       setTimeout(() => {
         this.addFood({
-          addFoodName: this.recommendationList[idx].nameZh,
+          nameZh: this.recommendationList[idx].nameZh,
           type: this.recommendationList[idx].type
         });
         this.recommendationList = this.recommendationList
@@ -215,12 +326,11 @@ export default {
           });
       });
       const res = await axios.post("/cabinet/userId/buy", buyItems);
-      if (res.status === 200) this.$router.push('/RefrigeratorList')
+      if (res.status === 200) this.$router.push("/RefrigeratorList");
     }
   },
   components: {
-    FoodBlock,
-    RecommendBlock
+    FoodBlock
   }
 };
 </script>
