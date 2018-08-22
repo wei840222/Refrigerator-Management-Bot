f14e0106da64ade36f22b0e65764c95de31b3053
萬俊瑋 <wei840222@gmail.com>
2018-08-22 22:09:02 +0800

    setting title

--- pages/Setting.vue
+++ pages/Setting.vue
@@ -1,3 +1,13 @@
 <template>
   <img src="img/setting.png" style="height: 100%; width: 100%;"/>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "提醒設定"
+    };
+  }
+};
+</script>
